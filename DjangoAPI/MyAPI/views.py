9015736b6b7e512c9411373a3897faac3fecc21f
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-10-02 14:42:53 +0600

    views done

--- DjangoAPI/MyAPI/views.py
+++ DjangoAPI/MyAPI/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
-# Create your views here.
+from . forms import MyForm
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from django.core import serializers
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from . models import approvals
+from . serializers import approvalsSerializers
+import pickle 
+from sklearn.externals import joblib
+import json
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
