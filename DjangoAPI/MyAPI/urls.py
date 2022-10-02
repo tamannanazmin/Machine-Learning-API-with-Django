b66c07bd821a8ff655ab9cf350239b8b880f1bc3
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-10-02 15:10:48 +0600

    ml model saved and views code updated

--- DjangoAPI/MyAPI/urls.py
+++ DjangoAPI/MyAPI/urls.py
@@ -1,7 +1,12 @@
 
+from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+from rest_framework import routers
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('form/', views.myform, name='myform'),
+    path('api/', include(routers.urls)),
+    path('status/', views.approvereject),
 ]
--- DjangoAPI/MyAPI/views.py
+++ DjangoAPI/MyAPI/views.py
@@ -15,3 +15,31 @@ import json
 import numpy as np
 from sklearn import preprocessing
 import pandas as pd
+
+# When called , get the all objects/data
+class ApprovalsView(viewsets.ModelViewSet):
+    queryset=approvals.objects.all()
+    serializer_class=approvalsSerializers
+def myform(request):
+    if request.method="POST":
+        form=MyForm(request.POST)
+        if form.is_valid():
+            myform=form.save(commit=False)
+    else:
+        form=MyForm()
+@api_view(["POST"])
+def approvereject(request):
+    try:
+        mdl=joblib.load("C:\Users\tamanna13505\ML_with_django\ML_CODE")
+        mydata=request.data
+        unit=np.array(list(mydata.values()))
+        unit=unit.reshape(1,-1)
+        scalers=joblib.load("")
+        X=scalers.transform(unit)
+        y_pred=mdl.predict(X)
+        y_pred=(y_pred>0.58)
+        newdf=pd.DataFrame(y_pred,columns=['Status'])
+        newdf=newdf.replace({True:'Approved', False:'Rejected'})
+        return JsonResponse('Your Status is {}'.format(newdf), safe=False)
+    except ValueError as e:
+        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
