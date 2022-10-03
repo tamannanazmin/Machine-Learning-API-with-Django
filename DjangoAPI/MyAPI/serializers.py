bf54e71db8113f6620b9678fd4a0fd8de7aa99d1
Tamanna Nazmin <42768952+tamannanazmin@users.noreply.github.com>
2022-10-03 16:43:47 +0600

    view error fixed

--- DjangoAPI/MyAPI/serializers.py
+++ DjangoAPI/MyAPI/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from rest_framework import approvals
+from . models import approvals
 
-class approvalsSerializers(serializers.ModelSerializers):
-    class Meta:
-        model=approvals
-        field='__all__'
+class approvalsSerializers(serializers.ModelSerializer):
+	class Meta:
+		model=approvals
+		fields='__all__'
--- DjangoAPI/MyAPI/views.py
+++ DjangoAPI/MyAPI/views.py
@@ -13,7 +13,7 @@ from rest_framework.parsers import JSONParser
 from . models import approvals
 from . serializers import approvalsSerializers
 import pickle
-from sklearn.externals import joblib
+import joblib
 import json
 import numpy as np
 from sklearn import preprocessing
@@ -24,7 +24,18 @@ import pandas as pd
 class ApprovalsView(viewsets.ModelViewSet):
 	queryset = approvals.objects.all()
 	serializer_class = approvalsSerializers
-
+def ohevalue(df):
+	ohe_col=joblib.load("C:/Users/tamanna13505/ML_with_django/DjangoAPI/MyAPI/allcol.pkl")
+	cat_columns=['Gender','Married','Education','Self_Employed','Property_Area']
+	df_processed = pd.get_dummies(df, columns=cat_columns)
+	newdict={}
+	for i in ohe_col:
+		if i in df_processed.columns:
+			newdict[i]=df_processed[i].values
+		else:
+			newdict[i]=0
+	newdf=pd.DataFrame(newdict)
+	return newdf
 '''def myform(request):
     if request.method="POST":
         form=MyForm(request.POST)
@@ -33,47 +44,46 @@ class ApprovalsView(viewsets.ModelViewSet):
     else:
         form=MyForm()'''
 #@api_view(["POST"])
-def approvereject(request):
-    try:
-        #ohe_col=joblib.load("C:\Users\tamanna13505\ML_with_django\DjangoAPI\MyAPI\loan_model.pkl")
-        mdl=joblib.load("C:/Users/tamanna13505/ML_with_django/DjangoAPI/MyAPI/loan_model.pkl")
-        mydata=request.data
-        unit=np.array(list(mydata.values()))
-        unit=unit.reshape(1,-1)
-        scalers=joblib.load("C:/Users/tamanna13505/ML_with_django/DjangoAPI/MyAPI/scalers.pkl")
-        X=scalers.transform(unit)
-        y_pred=mdl.predict(X)
-        y_pred=(y_pred>0.58)
-        newdf=pd.DataFrame(y_pred,columns=['Status'])
-        newdf=newdf.replace({True:'Approved', False:'Rejected'})
-        return JsonResponse('Your Status is {}'.format(newdf), safe=False)
-    except ValueError as e:
-        return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
+def approvereject(unit):
+	try:
+		mdl=joblib.load("/Users/tamanna13505/ML_with_django/DjangoAPI/MyAPI/loan_model.pkl")
+		scalers=joblib.load("/Users/tamanna13505/ML_with_django/DjangoAPI/MyAPI/scalers.pkl")
+		X=scalers.transform(unit)
+		y_pred=mdl.predict(X)
+		y_pred=(y_pred>0.58)
+		newdf=pd.DataFrame(y_pred, columns=['Status'])
+		newdf=newdf.replace({True:'Approved', False:'Rejected'})
+		#K.clear_session()
+		return (newdf.values[0][0],X[0])
+	except ValueError as e:
+		return (e.args[0])
         
 def cxcontact(request):
 	if request.method=='POST':
 		form=ApprovalForm(request.POST)
 		if form.is_valid():
-				firstname = form.cleaned_data['firstname']
-				lastname = form.cleaned_data['lastname']
-				dependants = form.cleaned_data['dependants']
-				applicantincome = form.cleaned_data['applicantincome']
-				coapplicatincome = form.cleaned_data['coapplicatincome']
-				loanamt = form.cleaned_data['loanamt']
-				loanterm = form.cleaned_data['loanterm']
-				credithistory = form.cleaned_data['credithistory']
-				gender = form.cleaned_data['gender']
-				married = form.cleaned_data['married']
-				graduatededucation = form.cleaned_data['graduatededucation']
-				selfemployed = form.cleaned_data['selfemployed']
-				area = form.cleaned_data['area']
-				#myDict = (request.POST).dict()
-				#df=pd.DataFrame(myDict, index=[0])
-				#answer=approvereject(ohevalue(df))[0]
-				#Xscalers=approvereject(ohevalue(df))[1]
-				#print(Xscalers)
-				#messages.success(request,'Application Status: {}'.format(answer))
+			Firstname = form.cleaned_data['firstname']
+			Lastname = form.cleaned_data['lastname']
+			Dependents = form.cleaned_data['Dependents']
+			ApplicantIncome = form.cleaned_data['ApplicantIncome']
+			CoapplicantIncome = form.cleaned_data['CoapplicantIncome']
+			LoanAmount = form.cleaned_data['LoanAmount']
+			Loan_Amount_Term = form.cleaned_data['Loan_Amount_Term']
+			Credit_History = form.cleaned_data['Credit_History']
+			Gender = form.cleaned_data['Gender']
+			Married = form.cleaned_data['Married']
+			Education = form.cleaned_data['Education']
+			Self_Employed = form.cleaned_data['Self_Employed']
+			Property_Area = form.cleaned_data['Property_Area']
+			print(Firstname, Lastname)
+			myDict = (request.POST).dict()
+			df=pd.DataFrame(myDict, index=[0])
+			print(ohevalue(df))
+			#answer=approvereject(ohevalue(df))[0]
+			#Xscalers=approvereject(ohevalue(df))[1]
+			#print(Xscalers)
+			#messages.success(request,'Application Status: {}'.format(answer))
 	
-	form=ApprovalForm()
+	form = ApprovalForm()
 				
 	return render(request, 'myform/cxform.html', {'form':form})
